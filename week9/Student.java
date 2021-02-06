0aecc87e3e370dc38ecfeec272f12d3b95969bdd
DaphnexxEli <wanatchaya_k@elearning.cmu.ac.th>
2021-02-06 18:09:29 +0700

    week9

--- week9/Student.java
+++ week9/Student.java
@@ -3,21 +3,23 @@ package week9;
 import java.util.Scanner;
 
 public class Student {
-    Scanner firstname = new Scanner(System.in);
-    Scanner lastname = new Scanner(System.in);
+    private String firstname = "Hello";
+    private String lastname = "World";
+    // Scanner firstname = new Scanner(System.in);
+    // Scanner lastname = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("please input you name: ");
-        System.out.println("please input your surname: ");
+        // System.out.println("please input you name: ");
+        // System.out.println("please input your surname: ");
         Student p = new Student();
         System.out.println(p.toString());
 
     }
 
-    public String toString() {
-        String name = firstname.nextLine();
-        String surname = lastname.nextLine();
-        return "My name is " + name + " " + surname;
+    // public String toString() {
+        // String name = firstname.nextLine();
+        // String surname = lastname.nextLine();
+    //     return "My name is " + firstname + " " + lastname;
 
-    }
+    // }
 }
